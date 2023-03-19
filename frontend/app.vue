ec48d40bcfde98739d7ccfc19c0c029b83af8eb1
devshark0814 <devshark0814@gmail.com>
2023-03-19 20:01:43 +0900

    Quasar追加

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+const samplelabel = "nuxt3";
+const sampleText= ref<string>("hello nuxt3");
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- Remove this component to get started! -->
+    <!--    <NuxtWelcome />-->
+    <q-btn :label="samplelabel" color="red"/>
+    <q-input filled v-model="sampleText" />
   </div>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  ssr: false,
+  target:"static",
+  css: [
+    'quasar/dist/quasar.prod.css'
+  ],
 })
